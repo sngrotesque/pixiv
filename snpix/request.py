af93cb191ad097477a60d60c6c767f17d768995c
SN-Grotesque <z7z-h@outlook.com>
2022-09-11 18:06:41 +0800

    Pixiv

--- snpix/request.py
+++ snpix/request.py
@@ -15,6 +15,7 @@ class pixiv:
         
         self.DEFINED_UserAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0'
         self.DEFINED_BQ = 24 # 基本量(Basic quantity)
+        self.DEFINED_HTTP_Headers = 
         
         self.RESULTS_ArtistNameID = {}
         self.RESULTS_ArtistArtworks = {}
