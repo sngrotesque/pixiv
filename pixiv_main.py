7995b9e0591f764d208d54c95281b33ec843240c
SN-Grotesque <z7z-h@outlook.com>
2022-09-11 13:04:17 +0800

    Pixiv

--- pixiv_main.py
+++ pixiv_main.py
@@ -8,7 +8,6 @@ Proxy = {'http': 'http://127.0.0.1:1080','https': 'http://127.0.0.1:1080'}
 res = snpix.pixiv(SNGrotesqueID, HTTP_Cookie=Cookie, Proxy = Proxy)
 res.GetAllUsersID
 
-
 print(res.RESULTS_AuthorNameID)
 
 # with open('000_ALL_Pixiv_Follow_list_new.json', 'w', encoding='utf-8') as f:
