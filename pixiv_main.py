169cf5a2c697c39d09dcbfa61b2693cfbcbe6c81
SN-Grotesque <z7z-h@outlook.com>
2022-09-11 18:10:21 +0800

    Pixiv

--- pixiv_main.py
+++ pixiv_main.py
@@ -6,7 +6,7 @@ Cookie = fread('000_cookie.txt').decode()
 Proxy = {'http': 'http://127.0.0.1:1080','https': 'http://127.0.0.1:1080'}
 
 res = snpix.pixiv(SNGrotesqueID, HTTP_Cookie=Cookie, Proxy = Proxy)
-res.GetAllUsersID
+res.GetAllUsersNameID
 
 with open('000_ALL_Pixiv_Follow_list_new.json', 'w', encoding='utf-8') as f:
     f.write(jdump(res.RESULTS_AuthorNameID, ensure_ascii=False))
