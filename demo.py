df86d8e7e1ccbbd91c2fa8076ed9d1a8ba2141e0
SN-Grotesque <z7z-h@outlook.com>
2022-09-19 13:01:11 +0800

    Pixiv

--- demo.py
+++ demo.py
@@ -1,14 +1,28 @@
 from snpix.utils import *
 import snpix
 
-YouOwnID = '' # 你自己的Pixiv ID
-Cookie = fread('000_cookie.txt').decode()
-Proxy = {'http': 'http://127.0.0.1:1080','https': 'http://127.0.0.1:1080'}
+YouOwnID = 000000000 # 你自己的Pixiv ID
+ArtistID = 000000000 # 指定的画师Pixiv ID
+Cookie = fread('Cookie.txt').decode()
+Proxy = {
+    'http': 'http://127.0.0.1:1080',
+    'https': 'http://127.0.0.1:1080'
+}
 
-res = snpix.pixiv(HTTP_Cookie=Cookie, Proxy = Proxy)
-res.GetAllUsersNameID(YouOwnID)
+# 实例化类
+pixiv = snpix.pixiv(HTTP_Cookie=Cookie, ArtistID = ArtistID,  Proxy = Proxy)
 
-with open('000_AllPixivFollowList.json', 'w', encoding='utf-8') as f:
-    f.write(jdump(res.RESULTS_ArtistNameID, ensure_ascii=False))
+pixiv.GetAllArtistInfo(YouOwnID) # 通过你自己的ID来获取关注列表中的所有用户ID和名字
+pixiv.RESULTS_ArtistInfo         # 结果保存在此处
 
+pixiv.GetTotalArtworksLink # 通过PixivAPI获取指定画师的所有插画作品链接
+pixiv.RESULTS_ArtworkLinks # 结果保存在此处
 
+artworkUrl = 'https://www.pixiv.net/artworks/99901109'
+pixiv.GetPictureLink(artworkUrl) # 通过PixivAPI获取单个作品中所有的图片链接
+pixiv.RESULTS_PictureLinks # 结果保存在此处
+
+PictureUrl = 'https://i.pximg.net/img-original/img/2022/07/22/06/21/36/99901109_p0.jpg'
+DirectoryName = 'pixiv_picture'
+pixiv.DownloadPictures(PictureUrl, DirectoryName)
+# 通过图片的源链接下载原图
