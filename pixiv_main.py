7a26c2325bc6ca28b1f827f5739538e49f449802
SN-Grotesque <z7z-h@outlook.com>
2022-10-01 03:28:29 +0800

    Multithread Pixiv

--- pixiv_main.py
+++ pixiv_main.py
@@ -1,15 +1,38 @@
 from snpix.utils import *
 import snpix
 
-import os, time
-
-artistID = '74188348'
-path = f'p:/Pixiv/{artistID}'
+# 000000 进度已完成
 
 cookie = fread('000_cookie.txt').decode()
 ProxyInfo = {"http": "http://127.0.0.1:1080", "https": "http://127.0.0.1:1080"}
 
-pixiv = snpix.pixiv(HTTP_Cookie=cookie, Proxy=ProxyInfo)
+def single():
+    artistID = ''
+    path = f'p:/Pixiv/{artistID}'
 
-pixiv.MultiThreadGetArtworks(artistID)
-pixiv.MultiThreadDownloadArtwork(pixiv.RESULTS_PictureLinks, path)
+    pixiv = snpix.pixiv(HTTP_Cookie=cookie, Proxy=ProxyInfo)
+
+    pixiv.MultiThreadGetArtworks(artistID)
+    pixiv.MultiThreadDownloadArtwork(pixiv.RESULTS_PictureLinks, path)
+
+def Multiple():
+    artistIDList = [
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        ''
+    ]
+
+    pixiv = snpix.pixiv(HTTP_Cookie=cookie, Proxy=ProxyInfo)
+    
+    for artistID in artistIDList:
+        path = f'p:/Pixiv/{artistID}'
+        pixiv.MultiThreadGetArtworks(artistID)
+        pixiv.MultiThreadDownloadArtwork(pixiv.RESULTS_PictureLinks, path)
+
+# Multiple()
+single()
